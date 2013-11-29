3d8baf236dfe8e122b6f59d43566161049b39ba7
Michael Bottjer <michaelbottjer@hotmail.com>
2013-11-29 09:43:04 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yelp::Application.config.secret_key_base = '609bd01e75f24d228a08f4398fd730df263df8df202a5d26cfa7a96d6aff00a578cc10a4330e80cf47e9ab601141b1ba049c7517835bc26f92cd950404407ec8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Yelp::Application.config.session_store :cookie_store, key: '_Yelp_session'
